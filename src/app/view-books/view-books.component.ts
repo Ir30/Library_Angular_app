7133c967ade303ab7ba6a77bad7b27c0be9fe73f
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-28 14:19:23 +0530

    ...

--- src/app/view-books/view-books.component.ts
+++ src/app/view-books/view-books.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-books.component.css']
 })
 export class ViewBooksComponent {
+    books=[
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/81FummIc2eL._AC_UL300_SR300,200_.jpg",
+        "title":"It Start With Us",
+        "price":"₹210"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg",
+        "title":"Atomic Habits",
+        "price":"₹369"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL600_SR600,400_.jpg",
+        "title":"IKIGAI",
+        "price":"₹410"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/71UwSHSZRnS._AC_UL600_SR600,400_.jpg",
+        "title":"The Power of Your Subconscious Mind",
+        "price":"₹414"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL600_SR600,400_.jpg",
+        "title":"The Psychology of Money",
+        "price":"₹398"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
+        "title":"It Ends With Us",
+        "price":"₹98"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/91+T1TAxkKL._AC_UL600_SR600,400_.jpg",
+        "title":"War Of Lanka",
+        "price":"₹219"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/81jv44QdNwL._AC_UL600_SR600,400_.jpg",
+        "title":"Grandmas Bag Of Stories",
+        "price":"₹368"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/61ZPDQOjw-L._AC_UL600_SR600,400_.jpg",
+        "title":"Do it Today",
+        "price":"₹98"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/61fp0RQR+9L._AC_UL600_SR600,400_.jpg",
+        "title":"The Almanack Of Naval Ravikant",
+        "price":"₹204"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL600_SR600,400_.jpg",
+        "title":"Rich Dad Poor Dad",
+        "price":"₹383"
+      },
+      {
+        "image":"https://images-eu.ssl-images-amazon.com/images/I/71ePXM6nOdL._AC_UL300_SR300,200_.jpg",
+        "title":"India Polity",
+        "price":"₹701"
+      }
 
+    ]
 }
